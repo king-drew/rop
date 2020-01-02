fc310dbfd4bc67e5c536bfb9f7316655255d1eb7
Drew Burchfiel <drew.burchfiel@gmail.com>
2020-01-02 05:03:29 -0600

    got started on Rust implementation. Next step is to implement a 'plus' function to combine error messages. (there is probably a better, more idiomatic way to combine errors)

--- fsharp/README.md
+++ fsharp/README.md
@@ -2,18 +2,4 @@
 
 A project to follow along with the article at https://fsharpforfunandprofit.com/posts/recipe-part2/
 
-| Concept | Description |
-| ------- | ----------- |
-| succeed | A constructor that takes a one-track value and creates a two-track value on the Success branch. In other contexts, this might also be called return or pure.
-| fail | constructor that takes a one-track value and creates a two-track value on the Failure branch.
-bind | An adapter that takes a switch function and creates a new function that accepts two-track values as input.
-| >= | An infix version of bind for piping two-track values into switch functions.
-| >> | composition. A combiner that takes two normal functions and creates a new function by connecting them in series.
-| >=> | Switch composition. A combiner that takes two switch functions and creates a new switch function by connecting them in series.
-| switch | An adapter that takes a normal one-track function and turns it into a switch function. (Also known as a "lift" in some contexts.)
-| map | An adapter that takes a normal one-track function and turns it into a two-track function. (Also known as a "lift" in some contexts.)
-| tee | adapter that takes a dead-end function and turns it into a one-track function that can be used in a data flow. (Also known as tap.)
-| tryCatch | An adapter that takes a normal one-track function and turns it into a switch function, but also catches exceptions.
-| doubleMap | An adapter that takes two one-track functions and turns them into a single two-track function. (Also known as bimap.)
-| plus | A combiner that takes two switch functions and creates a new switch function by joining them in "parallel" and "adding" the results. (Also known as ++ and <+> in other contexts.)
-| &&& | The "plus" combiner tweaked specifically for the validation functions, modelled on a binary AND.
+With dotnet 3.0 ```dotnet run``` will compile and run the Railway Oriented Programming example
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,91 @@
+
+fn switch<F, A, B, C>(f: F) -> impl Fn(A) -> Result<B, C>
+    where F: Fn(A) -> B
+{
+    move |x: A| Ok(f(x))
+}
+
+#[derive(Debug)]
+struct Request {
+    name: String,
+    email: String,
+}
+
+impl Request {
+    
+    fn err_from_str(s: &str) -> Result<Self, String> {
+        Err(String::from(s))
+    }
+
+    fn validate_name(self) -> Result<Self, String> {
+        if self.name == "" {
+            Self::err_from_str("Name must not be blank")
+        } else {
+            Ok(self)
+        }
+    }
+    
+    fn validate_name_len(self) -> Result<Self, String> {
+        if self.name.len() > 50 {
+            Self::err_from_str("Name must not be more than 50 chars")
+        } else {
+            Ok(self)
+        }
+    }
+
+    fn validate_email(self) -> Result<Self, String> {
+        if self.email == "" {
+            Self::err_from_str("Email must not be blank")
+        } else {
+            Ok(self)
+        }
+    }
+
+    fn combined_validation(self) -> Result<Self, String> {
+
+        // TODO: add `plus` function to combine errors or
+        // find more idiomatic approach
+
+        self.validate_name()
+            // and_then is basically bind
+            .and_then(|r| r.validate_name_len())
+            .and_then(|r| r.validate_email())
+    }
+
+    fn canonicalize_email(self) -> Self {
+        Request {email: self.email.to_lowercase(), ..self}
+    }
+
+    fn usecase(self) -> Result<Self, String> {
+        // this is a little wordy
+        // self.combined_validation()
+        //     .and_then(switch(|s: Self| s.canonicalize_email()))
+        // this is a little better
+        self.combined_validation()
+            .map(|s| s.canonicalize_email())
+    }
+
+    pub fn new<S, U>(name: S, email: U) -> Result<Self, String>
+        where S: Into<String>, U: Into<String> // multiple where clause just for fun
+    {
+        Request{name: name.into(), email: email.into()}
+            .usecase()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut l = Vec::new();
+    l.push(Request::new("Drew".to_string(), "Drew@Example.COM"));
+    l.push(Request::new("Drew", ""));
+    l.push(Request::new("", "Drew@Example.COM"));
+    l.push(Request::new("", ""));
+    l.push(Request::new("Drew jklsdfjksljkljklsdfjlksfjlsdkjflsdkjflskdjflkjdfjkdsjfldkjflsdkjf", "Drew@Example.COM"));
+    l.push(Request::new("Drew jklsdfjksljkljklsdfjlksfjlsdkjflsdkjflskdjflkjdfjkdsjfldkjflsdkjf", ""));
+    for r in l {
+        match r {
+            Ok(r) => println!("{:?}", r),
+            Err(e) => println!("{}", e),
+        }
+    }
 }
